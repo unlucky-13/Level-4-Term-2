4fe1f28b4e4ceb86c6be16204ada60f4407eba82
IslamMazharul <mazharbuet11@gmail.com>
2016-10-22 20:23:05 +0600

    Some comments added  :|

--- CSE472 Machine Learning Sessional/Assignment 3/ANN/src/ann/Main.java
+++ CSE472 Machine Learning Sessional/Assignment 3/ANN/src/ann/Main.java
@@ -16,8 +16,12 @@ public class Main {
         LoadData loadData = new LoadData() ;
         
 //          TRAINING PHASE          ////////////////////////////////////
-        LinkedList<Data> TrainData  = loadData.ReadTrainData() ;
-        Network network = new Network(TrainData.get(0).in.row,TrainData.get(0).out.row ,5, 4) ;
-        network.printWeightVectos(); 
+        LinkedList<Data> TrainData  = loadData.ReadTrainData() ; // the TrainData is a linked list of pair(x,y) where x is the input and y is the expected output for this input
+        Network network = new Network(TrainData.get(0).in.row,TrainData.get(0).out.row ,5, 4) ; 
+        // int  NeuronNo in the input layer 
+        // int NeuronNo in the output layer 
+        // int total layer no including hiddenlayer+inputlaye+outputlayer
+        // int No of Nerons in the hidden layers
+        network.printWeightVectos();  // after randomly initializaing printing the wieght vecs in between the layers 
     }
 }
